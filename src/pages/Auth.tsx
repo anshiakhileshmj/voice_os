3a25c39d29d76691b123acbdec2624d7f137c5a8
MJAK <yamrendra378@gmail.com>
2025-07-25 19:49:44 +0530

    Create Auth.tsx

---
+++ src/pages/Auth.tsx
@@ -0,0 +1,12 @@
+
+import AuthForm from '@/components/AuthForm';
+
+const Auth = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 via-indigo-50 to-purple-50 flex items-center justify-center p-4">
+      <AuthForm />
+    </div>
+  );
+};
+
+export default Auth;
